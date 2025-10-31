b6c086c464a1bdbd9cbafd48a2443fe4a5aaf95b
dalianassar <daliayassernassar@gmail.com>
2025-11-01 00:40:02 +0200

    add

--- alx-react-app/src/App.jsx
+++ alx-react-app/src/App.jsx
@@ -1,17 +1,32 @@
-import { useState } from 'react'
-import reactLogo from './assets/react.svg'
-import viteLogo from '/vite.svg'
-import './App.css'
+// import { useState } from 'react'
+// import reactLogo from './assets/react.svg'
+// import viteLogo from '/vite.svg'
+// import './App.css'
+// function App() {
+//   return (
+//     <div className="App">
+//       <WelcomeMessage />
+//       {/* Other content can go here */}
+//     </div>
+//   );
+// }
+
+import WelcomeMessage from './components/WelcomeMessage';
+import Header from './components/Header';
+import MainContent from './components/MainContent';
+import Footer from './components/Footer';
+
+
 function App() {
   return (
     <div className="App">
       <WelcomeMessage />
-      {/* Other content can go here */}
+      <Header />
+      <MainContent />
+      <Footer />
     </div>
   );
 }
 
-import WelcomeMessage from './components/WelcomeMessage';
+export default App;
 
-
-export default App
